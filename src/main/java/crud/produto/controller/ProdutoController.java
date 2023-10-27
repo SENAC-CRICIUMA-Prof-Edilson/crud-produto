33d4f6b60f471913e4432768da0310d28e10db90
Edilson Bitencourt <theusdido@hotmail.com>
2023-10-27 20:13:37 -0300

    Listagem de Produtos

--- src/main/java/crud/produto/controller/ProdutoController.java
+++ src/main/java/crud/produto/controller/ProdutoController.java
@@ -1,9 +1,11 @@
 package crud.produto.controller;
 
-import javax.swing.text.html.Option;
+import java.util.List;
 
+import javax.swing.text.html.Option;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,5 +28,12 @@ public class ProdutoController {
     public String salvarProduto(@ModelAttribute ProdutoEntity produto){        
         repositorio.save(produto);
         return "produto/response";
+    }
+
+    @GetMapping(value = "/produto/listar")
+    public String listarProduto(Model listar){
+        List<ProdutoEntity> produtos = repositorio.findAll();
+        listar.addAttribute("produtos",produtos);
+        return "produto/listar";
     }
 }
