44a1e48289d8598e0f5a1df212a0eeb172ba76c0
Edilson Bitencourt <theusdido@hotmail.com>
2023-10-27 19:22:21 -0300

    Cadastro de Produto

--- src/main/java/crud/produto/controller/ProdutoController.java
+++ src/main/java/crud/produto/controller/ProdutoController.java
@@ -3,24 +3,28 @@ package crud.produto.controller;
 import javax.swing.text.html.Option;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import crud.produto.model.ProdutoEntity;
 import crud.produto.repository.ProdutoRepository;
 
+@Controller
 public class ProdutoController {
     
     @Autowired
     private ProdutoRepository repositorio;
 
+    @GetMapping(value="/produto")
+    public String produtoPage(){
+        return "produto/form";
+    }
+
     @PostMapping(value="/produto/salvar")
-    public boolean salvar(ProdutoEntity produto){
-        //try{
-            repositorio.save(produto);
-            return true;
-        //}catch(Exception e){
-        //    return false;
-        //}
+    public String salvarProduto(@ModelAttribute ProdutoEntity produto){        
+        repositorio.save(produto);
+        return "produto/response";
     }
 }
